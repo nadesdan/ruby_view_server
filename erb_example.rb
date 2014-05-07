03c5d6c2cb67c7ed978048f26d5deb392e801da1
Daniel Benjamin <nadesdan@gmail.com>
2014-05-07 13:25:32 +0200

    lesson part 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,9 @@
 require 'erb'
-
+name = []
+firstname = "Daniel"
+lastname = "Benjamin"
+age = 36
+name = [firstname, lastname]
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new "My name is <%= name.first %> <%= name.last %>"
 puts template.result(binding)
